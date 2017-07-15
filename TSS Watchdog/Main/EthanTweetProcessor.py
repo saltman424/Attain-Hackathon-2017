67adde79ad7a31220b629f4cd6c8ac7546e13f03
EtonMon <etonxt@gmail.com>
2017-07-15 12:29:43 -0400

    merging

--- TSS Watchdog/Main/EthanTweetProcessor.py
+++ TSS Watchdog/Main/EthanTweetProcessor.py
@@ -1,2 +1,34 @@
+import tweepy
+import nltk
+import TweetClassifier as tc
+import Helper as h
+import io
+
+countedWords = dict()
+
 def process_status(status):
-    pass
+    logfile = open("newlog.txt", "w")
+    if not status.retweeted and "RT @" not in status.text:
+        print(status.text)
+        if tc.is_suspicious(status):
+            tokenized = nltk.word_tokenize(status.text)
+
+            for word in tokenized:
+                if word in countedWords: 
+                    x = countedWords[word]
+                    countedWords[word] = x + 1
+                else:
+                    countedWords[word] = 1
+
+            sortedList = sorted(countedWords.items(), key=lambda x: x[1], reverse=True)
+            print(sortedList)
+            convertToString = ",".join("(%s,%s)" % tup for tup in sortedList)
+
+            with io.open("testfile", "w", encoding="utf-8") as file:
+                file.write(convertToString)
+        print("")
+
+
+
+
+
--- TSS Watchdog/Main/TweetScanner.py
+++ TSS Watchdog/Main/TweetScanner.py
@@ -9,17 +9,17 @@ auth1 = tweepy.OAuthHandler("i4SQAovlLNQhflgfFy1pMVVT6",                        
 auth1.set_access_token("886035091448893442-hOwTGGgxrbWyO72GQPI48I9WBkCVhsd",            # Access token
                           "Vb2iNiH266zAwYr6J8iNLEflS4h8JS8KlUY6JbPFoOCun")              # Access Secret
 
-auth2 = tweepy.OAuthHandler("i4SQAovlLNQhflgfFy1pMVVT6",                                # Consumer Key
-                                "F0FK9sOkOD3DmOMOivPESfQP2Vx6da2addUE0FApd1qVurtybK")   # Consumer Secret
-auth2.set_access_token("886035091448893442-hOwTGGgxrbWyO72GQPI48I9WBkCVhsd",            # Access token
-                          "Vb2iNiH266zAwYr6J8iNLEflS4h8JS8KlUY6JbPFoOCun")              # Access Secret
+auth2 = tweepy.OAuthHandler("4gZUSeRuqL2UtxaTuE6VzAokz",                                # Consumer Key
+                                "OHwKB9GVSnhX8iOKSJt6w8ipWpxm6em1c2WiqIE6tHSZ7V6oc0")   # Consumer Secret
+auth2.set_access_token("886115828797779968-P0bFhQC1o7lLihY4IvHUtdIMTOz51oi",            # Access token
+                          "YDQ5wNQINZGVyoCaZ9weh72QyEj2RXk186XLrfYx6SPCh")              # Access Secret
 
 curr_auth = auth2
 
 
 class ScanningStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        tp.process_status(status)
+        etp.process_status(status)
 
     def on_error(self, status_code):
         print("Error while trying to stream! (Error code: ",status_code,")")
