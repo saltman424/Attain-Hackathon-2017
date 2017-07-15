efed8b3fe4f7f95d5837c1a1654547f798cc213a
saltman424 <saltman424@gmail.com>
2017-07-15 17:15:21 -0400

    fixed json

--- TSS Watchdog/Backend/UserClassifier.py
+++ TSS Watchdog/Backend/UserClassifier.py
@@ -20,13 +20,18 @@ def is_suspicious(user):
     wordcloud_results = wordcloud_score(user, tweets)
     score += wordcloud_results[0]
 
+    average_sentiment_results = average_sentiment_score(user, tweets)
+    score += average_sentiment_results[0]
+
     return score >= threshold_score, score
 
 
 def average_buzzword_score(user, tweets):
     total_score = 0
+
     for tweet in tweets:
         total_score += tc.buzzword_score(tweet.text)[0]
+
     return total_score / len(tweets), "Sorry no information to give right now"
 
 
@@ -38,6 +43,14 @@ def wordcloud_score(user, tweets):
         wordcloud.add_words_from(tweet.text)
 
     return max_score * wordcloud.similarity_to(tp.basis_wordcloud), "Sorry no information to give right now"
+
+def average_sentiment_score(user, tweets):
+    total_score = 0
+
+    for tweet in tweets:
+        total_score += tc.sentiment_score(tweet.text)[0]
+
+    return total_score / len(tweets), "Sorry no information to give right now"
 
 
 def profile_nn_score(user):
